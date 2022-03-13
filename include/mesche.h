a90107b14ad5d5b9cb0d2288ef746fd20b5c074c
David Wilson <david@daviwil.com>
2022-03-13 16:05:45 +0200

    Add fs.h and string.h to external mesche.h

--- include/mesche.h
+++ include/mesche.h
@@ -1,9 +1,11 @@
 #ifndef _mesche_h
 #define _mesche_h
 
-#include "../src/object.h"
+#include "../src/fs.h"
 #include "../src/module.h"
-#include "../src/vm.h"
+#include "../src/object.h"
 #include "../src/repl.h"
+#include "../src/string.h"
+#include "../src/vm.h"
 
 #endif
