b74266de1cb5b72bfe77f78cf0397649531b0ca4
David Wilson <david@daviwil.com>
2022-03-15 05:35:29 +0200

    list: Add `append` native function

--- src/list.c
+++ src/list.c
@@ -43,11 +43,33 @@ Value list_pair_p_msc(MescheMemory *mem, int arg_count, Value *args) {
   return BOOL_VAL(mesche_object_is_kind(args[0], ObjectKindCons));
 }
 
+Value list_append_msc(MescheMemory *mem, int arg_count, Value *args) {
+  // We can just return the second parameter if the first is an empty list
+  if (IS_EMPTY(args[0])) {
+    return args[1];
+  }
+
+  // Loop to the end of the first cons and set its cdr to the second cons
+  Value current = args[0];
+  while (IS_CONS(current)) {
+    ObjectCons *cons = AS_CONS(current);
+    if (IS_EMPTY(cons->cdr)) {
+      cons->cdr = args[1];
+      break;
+    }
+
+    current = cons->cdr;
+  }
+
+  return args[0];
+}
+
 void mesche_list_module_init(VM *vm) {
   mesche_vm_define_native_funcs(vm, "mesche list",
                                 &(MescheNativeFuncDetails[]){{"list-nth", list_nth_msc, true},
                                                              {"pair?", list_pair_p_msc, true},
                                                              {"car", list_car_msc, true},
                                                              {"cdr", list_cdr_msc, true},
+                                                             {"append", list_append_msc, true},
                                                              {NULL, NULL, false}});
 }
