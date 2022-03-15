a7dfacd7e02a05f1f16ada90912fc8a03b3afd2e
David Wilson <david@daviwil.com>
2022-03-15 07:34:10 +0200

    Add initial working implementation of `mesche build` and project.msc

--- src/main.c
+++ src/main.c
@@ -1,17 +1,31 @@
+#include <errno.h>
 #include <mesche.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 int main(int argc, char **argv) {
   VM vm;
   mesche_vm_init(&vm, argc, argv);
 
-  // Set up load paths and load core modules
-  mesche_vm_load_path_add(&vm, "./compiler/modules/");
-  mesche_vm_load_path_add(&vm, "./modules/");
+  // Set up the compiler's load path relative to the program path
+  char cwd[500];
+  char *program_path = mesche_fs_resolve_path(argv[0]);
+  char *program_dir = mesche_fs_file_directory(program_path);
+  char *original_dir = getcwd(&cwd[0], sizeof(cwd));
+
+  // Change directory to resolve the path correctly
+  chdir(program_dir);
+  char *main_file_path = mesche_fs_resolve_path("./../src/main.msc");
+  char *modules_path = mesche_fs_resolve_path("./../modules/");
+  chdir(original_dir);
+
+  mesche_vm_load_path_add(&vm, modules_path);
   mesche_vm_register_core_modules(&vm);
 
-  // Evaluate the init script
-  mesche_vm_load_file(&vm, "src/main.msc");
+  // Evaluate the main entrypoint file
+  mesche_vm_load_file(&vm, main_file_path);
 
   // Report the final memory allocation statistics
   /* mesche_mem_report((MescheMemory *)&vm); */
@@ -21,5 +35,11 @@ int main(int argc, char **argv) {
 
   // Free the VM and exit
   mesche_vm_free(&vm);
+
+  // Free temporary strings
+  free(main_file_path);
+  free(modules_path);
+  free(program_path);
+
   return 0;
 }
