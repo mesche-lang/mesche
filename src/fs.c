f93aded882603d7cba188e9abbbb3a489048ed87
David Wilson <david@daviwil.com>
2022-03-17 11:44:32 +0200

    fs: Improve `path-resolve` algorithm, add `path-ensure` function

--- src/fs.c
+++ src/fs.c
@@ -1,9 +1,11 @@
+#include <errno.h>
+#include <libgen.h>
+#include <linux/limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-#include <libgen.h>
 #include <unistd.h>
 
 #include "fs.h"
@@ -16,31 +18,43 @@ bool mesche_fs_path_absolute_p(const char *fs_path) { return fs_path[0] == '/'; 
 
 // NOTE: Caller must free the result string!
 char *mesche_fs_resolve_path(const char *fs_path) {
-  if (mesche_fs_path_absolute_p(fs_path)) {
-    // Return a copy of the string that can be freed so that the caller can deal
-    // with it consistently
-    return strdup(fs_path);
+  // Allocate a buffer and make it an empty string
+  char path_buf[PATH_MAX];
+  path_buf[0] = '\0';
+
+  // If the path itself is not absolute, start from the current directory
+  if (fs_path[0] != '/') {
+    getcwd(&path_buf[0], sizeof(path_buf));
   }
 
-  char *current_path = getcwd(NULL, 0);
-  if (current_path != NULL) {
-    size_t size = strlen(current_path) + strlen(fs_path) + 2;
-    char *joined_path = malloc(size);
-    snprintf(joined_path, size, "%s/%s", current_path, fs_path);
+  // Now we have our starting path, loop through it and find each new part of the path
+  // We start after the current path since it must already exist.
+  int path_start = strlen(path_buf) + 1;
+  path_buf[path_start - 1] = '/';
+  path_buf[path_start] = '\0';
 
-    // Resolve the joined path and free the intermediate strings
-    char *resolved_path = realpath(joined_path, NULL);
-    free(current_path);
-    free(joined_path);
-    return resolved_path;
-  } else {
-    PANIC("Error while calling getcwd()");
+  for (int i = 0; i < strlen(fs_path); i++) {
+    path_buf[path_start + i] = fs_path[i];
+    path_buf[path_start + i + 1] = '\0';
+
+    if (fs_path[i] == '/') {
+      if (realpath(path_buf, &path_buf[0]) != NULL) {
+        path_start = strlen(path_buf) + 1;
+        path_buf[path_start - 1] = '/';
+        path_buf[path_start] = '\0';
+        path_start -= i + 1; // Adjust for future character placements
+      } else {
+        // We've reached a part of the path that can't be resolved but
+        // we'll keep copying until the end of the string since we don't
+        // expect the full path to exist.
+      }
+    }
   }
+
+  return strdup(path_buf);
 }
 
-char *mesche_fs_file_directory(const char *file_path) {
-  return dirname(file_path);
-}
+char *mesche_fs_file_directory(const char *file_path) { return dirname((char *)file_path); }
 
 char *mesche_fs_file_read_all(const char *file_path) {
   FILE *file = fopen(file_path, "rb");
@@ -79,7 +93,8 @@ Value fs_path_exists_p_msc(MescheMemory *mem, int arg_count, Value *args) {
 
 Value fs_path_resolve_msc(MescheMemory *mem, int arg_count, Value *args) {
   char *resolved_path = mesche_fs_resolve_path(AS_CSTRING(args[0]));
-  ObjectString *resolved_path_str = mesche_object_make_string(mem, resolved_path, strlen(resolved_path));
+  ObjectString *resolved_path_str =
+      mesche_object_make_string(mem, resolved_path, strlen(resolved_path));
   free(resolved_path);
 
   return OBJECT_VAL(resolved_path_str);
@@ -128,21 +143,68 @@ Value fs_file_modified_time_msc(MescheMemory *mem, int arg_count, Value *args) {
   return NUMBER_VAL(file_stat.st_mtime);
 }
 
-Value fs_directory_create_msc(MescheMemory *mem, int arg_count, Value *args) {
+int fs_directory_create(const char *dir_path) {
   // TODO: Make permissions a parameter
-  int result = mkdir(AS_CSTRING(args[0]), 0777);
+  return mkdir(dir_path, 0777);
+}
 
+Value fs_directory_create_msc(MescheMemory *mem, int arg_count, Value *args) {
   // TODO: Write out error on failure
+  int result = fs_directory_create(AS_CSTRING(args[0]));
 
   return result == 0 ? T_VAL : NIL_VAL;
 }
 
+Value fs_path_ensure_msc(MescheMemory *mem, int arg_count, Value *args) {
+  // Ensure we're looking at a string
+  if (!IS_STRING(args[0])) {
+    // TODO: Throw an error
+    PANIC("Received a non-string input: %d\n", AS_OBJECT(args[0])->kind);
+  }
+
+  char *resolved_path = mesche_fs_resolve_path(AS_CSTRING(args[0]));
+
+  // Step through the path making sure each part exists
+  int path_len = strlen(resolved_path);
+  for (int i = 0; i < path_len; i++) {
+    if (resolved_path[i] == '/' || i + 1 == path_len) {
+      // Temporarily terminate the string at this location
+      char tmp = resolved_path[i + 1];
+      resolved_path[i + 1] = '\0';
+
+      // Create the directory if it doesn't already exist
+      if (!mesche_fs_path_exists_p(resolved_path)) {
+        if (fs_directory_create(resolved_path) == -1) {
+          // TODO: Throw a language error instead
+          if (errno == EEXIST) {
+            printf("A file with the same name already exists where a directory is being created: %s\n", errno, resolved_path);
+            return NIL_VAL;
+          } else {
+            printf("Error %d while creating path: %s\n", errno, resolved_path);
+            return NIL_VAL;
+          }
+        }
+      }
+
+      // Restore the previous character
+      resolved_path[i + 1] = tmp;
+    }
+  }
+
+  // Create a resolved string to be returned
+  ObjectString *resolved_path_str =
+      mesche_object_make_string(mem, resolved_path, strlen(resolved_path));
+  free(resolved_path);
+
+  return OBJECT_VAL(resolved_path_str);
+}
 
 void mesche_fs_module_init(VM *vm) {
   mesche_vm_define_native_funcs(
       vm, "mesche fs",
       &(MescheNativeFuncDetails[]){{"path-exists?", fs_path_exists_p_msc, true},
                                    {"path-resolve", fs_path_resolve_msc, true},
+                                   {"path-ensure", fs_path_ensure_msc, true},
                                    {"file-name", fs_file_name_msc, true},
                                    {"file-directory", fs_file_directory_msc, true},
                                    {"file-extension", fs_file_extension_msc, true},
