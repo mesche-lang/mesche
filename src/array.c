40df6c4519cbb38b1f72a9726627955479cb0176
David Wilson <david@daviwil.com>
2022-03-22 19:09:31 +0200

    array: Add `array-nth-set!` and add length argument to `make-array`

--- src/array.c
+++ src/array.c
@@ -9,6 +9,16 @@ Value mesche_array_push(MescheMemory *mem, ObjectArray *array, Value value) {
 
 Value array_make_msc(MescheMemory *mem, int arg_count, Value *args) {
   ObjectArray *array = mesche_object_make_array((VM *)mem);
+
+  if (arg_count == 1) {
+    // Initialize the array to the specified size
+    // TODO: Type check the argument
+    int length = AS_NUMBER(args[0]);
+    for (int i = 0; i < length; i++) {
+      mesche_array_push(mem, array, NIL_VAL);
+    }
+  }
+
   return OBJECT_VAL(array);
 }
 
@@ -45,6 +55,18 @@ Value array_nth_msc(MescheMemory *mem, int arg_count, Value *args) {
   return array->objects.values[(int)AS_NUMBER(value)];
 }
 
+Value array_nth_set_msc(MescheMemory *mem, int arg_count, Value *args) {
+  if (arg_count != 3) {
+    PANIC("Function requires 3 parameters.");
+  }
+
+  ObjectArray *array = AS_ARRAY(args[0]);
+  Value index = args[1];
+
+  array->objects.values[(int)AS_NUMBER(index)] = args[2];
+  return args[2];
+}
+
 void mesche_array_module_init(VM *vm) {
   mesche_vm_define_native_funcs(
       vm, "mesche array",
@@ -52,5 +74,6 @@ void mesche_array_module_init(VM *vm) {
                                    {"array-push", array_push_msc, true},
                                    {"array-length", array_length_msc, true},
                                    {"array-nth", array_nth_msc, true},
+                                   {"array-nth-set!", array_nth_set_msc, true},
                                    {NULL, NULL, false}});
 }
