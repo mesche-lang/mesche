b17175b240ff333f688ac7a15709768a6ebca53e
David Wilson <david@daviwil.com>
2022-03-31 11:54:48 +0300

    math: Add `min` and `max` functions

--- src/math.c
+++ src/math.c
@@ -20,12 +20,32 @@ Value math_rand_int_msc(MescheMemory *mem, int arg_count, Value *args) {
   return NUMBER_VAL(rand() % (int)AS_NUMBER(args[0]));
 }
 
+Value math_min_msc(MescheMemory *mem, int arg_count, Value *args) {
+  if (arg_count != 2) {
+    PANIC("Function requires 2 parameters.");
+  }
+
+  // TODO: Add type checks
+  return NUMBER_VAL(fmin(AS_NUMBER(args[0]), AS_NUMBER(args[1])));
+}
+
+Value math_max_msc(MescheMemory *mem, int arg_count, Value *args) {
+  if (arg_count != 2) {
+    PANIC("Function requires 2 parameters.");
+  }
+
+  // TODO: Add type checks
+  return NUMBER_VAL(fmax(AS_NUMBER(args[0]), AS_NUMBER(args[1])));
+}
+
 void mesche_math_module_init(VM *vm) {
   // TODO: Add an API for setting the seed
   srand(time(NULL));
 
   mesche_vm_define_native_funcs(vm, "mesche math",
                                 (MescheNativeFuncDetails[]){{"floor", math_floor_msc, true},
+                                                            {"min", math_min_msc, true},
+                                                            {"max", math_max_msc, true},
                                                             {"rand-int", math_rand_int_msc, true},
                                                             {NULL, NULL, false}});
 }
