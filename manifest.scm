512590e96c0c3c6011615e8294f2775890e896a4
David Wilson <david@daviwil.com>
2022-09-08 12:42:39 +0300

    Add manifest.scm

---
+++ manifest.scm
@@ -0,0 +1,26 @@
+(specifications->manifest
+ '( ;; C/C++
+   "bash"
+   "coreutils"
+   "gcc-toolchain"
+   "make"
+   "pkg-config"
+   "gdb"
+   "cmake"
+   "zlib"
+
+   ;; Command line tools
+   "git"
+   "tar"
+   "wget"
+   "gzip"
+   "openssh"
+   "nss-certs"
+
+   ;; OpenGL and GLFW
+   "mesa"
+   "glfw"
+
+   ;; Font rendering
+   "freetype"
+   "fontconfig"))
