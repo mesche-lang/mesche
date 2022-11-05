590986693e635b4cb41e6faa08a5118164fb3528
David Wilson <david@daviwil.com>
2022-11-05 16:14:59 +0200

    mesche.h: Add some missing API types

--- components/compiler/include/mesche.h
+++ components/compiler/include/mesche.h
@@ -1,8 +1,11 @@
 #ifndef _mesche_h
 #define _mesche_h
 
+#include "../src/array.h"
 #include "../src/fs.h"
+#include "../src/gc.h"
 #include "../src/module.h"
+#include "../src/native.h"
 #include "../src/object.h"
 #include "../src/process.h"
 #include "../src/repl.h"
