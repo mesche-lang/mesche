b1094aeec5b3ca37c99eaca74a105bbe23a72422
David Wilson <david@daviwil.com>
2022-11-13 17:39:38 +0200

    reader: Initialize file_name when not in use

--- components/compiler/src/reader.c
+++ components/compiler/src/reader.c
@@ -31,6 +31,7 @@ void mesche_reader_from_file(ReaderContext *context, Reader *reader, const char 
 
 void mesche_reader_from_string(ReaderContext *context, Reader *reader, const char *source) {
   reader->context = context;
+  reader->file_name = NULL;
   mesche_scanner_init(&reader->scanner, source);
 }
 
