c950115e4ae52184b3bf52f7511a3b966aa1047a
David Wilson <david@daviwil.com>
2022-11-26 16:47:04 +0200

    mesche-emacs: Add custom REPL command `mesche-eval-defun`

--- components/mesche-emacs/mesche.el
+++ components/mesche-emacs/mesche.el
@@ -3,6 +3,7 @@
 
 (require 'transient)
 (require 'comint)
+(require 'project)
 
 (define-derived-mode mesche-mode scheme-mode "Mesche"
   "Major mode for editing Mesche code."
@@ -85,10 +86,15 @@
     ("n" "new" (lambda () (interactive) (message "hello")))
     ("v" "version" (lambda () (interactive) (message "hello")))]])
 
+(defvar mesche-repl-buffer-name "*mesche: repl*")
+
 (defun run-mesche ()
   "Run the Mesche REPL inside an interactive buffer."
   (interactive)
-  (let* ((buffer (get-buffer-create "*mesche: repl*"))
+  (let* ((default-directory (if (project-current)
+                                (project-root (project-current))
+                              default-directory))
+         (buffer (get-buffer-create mesche-repl-buffer-name))
          (proc-alive (comint-check-proc buffer))
          (process (get-buffer-process buffer)))
     ;; if the process is dead then re-create the process and reset the
@@ -102,5 +108,44 @@
     ;; Regardless, provided we have a valid buffer, we pop to it.
     (when buffer
       (pop-to-buffer buffer))))
+
+(defconst mesche--module-regexp "^(define-module \\((.*)\\)")
+
+(defun mesche--get-module-current-buffer ()
+  "Find current module, or normalize MODULE."
+  (save-excursion
+    (beginning-of-buffer)
+    (if (re-search-forward geiser-mesche--module-regexp nil t)
+        (car (geiser-syntax--read-from-string (match-string-no-properties 1)))
+      nil)))
+
+;; Eval a region or top-level expression in the context of a module
+(defun mesche-eval-region ()
+  (message "EVAL REGION: %s" (buffer-substring-no-properties
+                              (region-beginning)
+                              (region-end))))
+
+(defun mesche-eval-defun ()
+  (interactive)
+  (save-mark-and-excursion
+    (let* ((module (mesche--get-module-current-buffer))
+           (defun-string (progn
+                           (mark-defun)
+                           (format "(begin\n(module-enter %S)\n%s)"
+                                   (if module module '(mesche-user))
+                                   (buffer-substring-no-properties
+                                    (region-beginning)
+                                    (region-end)))))
+           (repl-buffer (get-buffer mesche-repl-buffer-name)))
+        (if repl-buffer
+            (progn
+              ;; Switch to the repl buffer and delete existing input
+              (switch-to-buffer repl-buffer)
+              (comint-delete-input)
+              (comint-bol)
+              (insert (string-replace "\n" " " defun-string))
+              (comint-send-input))))))
+
+(define-key mesche-mode-map (kbd "C-M-x") #'mesche-eval-defun)
 
 (provide 'mesche)
